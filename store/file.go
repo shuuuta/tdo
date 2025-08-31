176f3ec8723814b8da187d7b0ff9d1e6e6506b80
Shuta Fujii <shuuutaf@gmail.com>
2025-08-31 15:07:16 +0900

    feat: add global save and load functions

--- store/file.go
+++ store/file.go
@@ -9,12 +9,6 @@ import (
 	"github.com/shuuuta/tdo/utils"
 )
 
-//// プロジェクト取得
-//func LoadProject(path string) (*model.Project, error)
-//
-//// プロジェクト保存
-//func SaveProject(project *model.Project) error
-//
 //// タスク追加
 //func AddTask(projectPath, title string) (*model.Task, error)
 //
@@ -23,10 +17,6 @@ import (
 //
 //// プロジェクト横断タスク取得（-a オプション用）
 //func LoadAllProjects() ([]*model.Project, error)
-//
-//// グローバルタスク読み書き
-//func LoadGlobalTasks() ([]model.Task, error)
-//func AddGlobalTask(title string) (*model.Task, error)
 
 func LoadProject(projectPath, saveDir string) (*model.Project, error) {
 	var p model.Project
@@ -48,7 +38,7 @@ func LoadProject(projectPath, saveDir string) (*model.Project, error) {
 	return &p, nil
 }
 
-func SaveProject(project *model.Project, path string) error {
+func SaveProject(project *model.Project, saveDir string) error {
 	j, err := json.MarshalIndent(project, "", "  ")
 	if err != nil {
 		return err
@@ -59,7 +49,35 @@ func SaveProject(project *model.Project, path string) error {
 		return err
 	}
 
-	fpath := filepath.Join(path, h+".json")
+	fpath := filepath.Join(saveDir, h+".json")
+	if err := os.WriteFile(fpath, j, 0644); err != nil {
+		return err
+	}
+	return nil
+}
+
+func LoadGlobal(saveDir string) (*model.Project, error) {
+	var p model.Project
+
+	d, err := os.ReadFile(filepath.Join(saveDir, "global.json"))
+	if err != nil {
+		return &p, err
+	}
+
+	if err := json.Unmarshal(d, &p); err != nil {
+		return &p, err
+	}
+
+	return &p, nil
+}
+
+func SaveGlobal(project *model.Project, saveDir string) error {
+	j, err := json.MarshalIndent(project, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fpath := filepath.Join(saveDir, "global.json")
 	if err := os.WriteFile(fpath, j, 0644); err != nil {
 		return err
 	}
--- store/file_test.go
+++ store/file_test.go
@@ -43,3 +43,37 @@ func TestSaveAndLoadProject(t *testing.T) {
 		t.Fatalf("expect %+v, got %+v", &project, got)
 	}
 }
+
+func TestSaveAndLoadGlobal(t *testing.T) {
+	now := time.Now().UTC()
+	task1 := model.Task{
+		ID:        1,
+		Title:     "task 1",
+		CreatedAt: now,
+	}
+	task2 := model.Task{
+		ID:        2,
+		Title:     "task 2",
+		CreatedAt: now,
+	}
+	project := model.Project{
+		Tasks: []model.Task{
+			task1,
+			task2,
+		},
+	}
+
+	tmpDir := t.TempDir()
+
+	if err := SaveGlobal(&project, tmpDir); err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := LoadGlobal(tmpDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !cmp.Equal(got, &project) {
+		t.Fatalf("expect %+v, got %+v", &project, got)
+	}
+}
