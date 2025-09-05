229d46a9a573cd2900e3cbc130cc67f458a5f501
Shuta Fujii <shuuutaf@gmail.com>
2025-09-05 19:53:47 +0900

    feat: add AddTask func

--- store/task.go
+++ store/task.go
@@ -1,11 +1,57 @@
 package store
 
-// // タスク追加
-// func AddTask(projectPath, title string) (*model.Task, error)
-func AddTask() {
-	// load or create project json
+import (
+	"os"
+	"time"
 
-	// save file
+	"github.com/shuuuta/tdo/model"
+)
+
+func AddTask(projectPath, title string) (*model.Task, error) {
+	t := model.Task{}
+
+	p, err := LoadProject(projectPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			t.ID = 0
+			t.Title = title
+			t.CreatedAt = time.Now().UTC()
+
+			p := model.Project{
+				ProjectPath: projectPath,
+				IsGlobal:    false,
+				Tasks:       []model.Task{t},
+			}
+
+			if err := SaveProject(&p); err != nil {
+				return &t, err
+			}
+			return &t, nil
+		} else {
+			return &t, err
+		}
+	}
+
+	id := 0
+	for _, v := range p.Tasks {
+		if id <= v.ID {
+			id = v.ID + 1
+		}
+	}
+
+	t.ID = id
+	t.Title = title
+	t.CreatedAt = time.Now().UTC()
+
+	p.Tasks = append(p.Tasks, t)
+
+	SaveProject(p)
+
+	return &t, nil
+}
+
+func AddGlobalTask(title string) (*model.Task, error) {
+	return nil, nil
 }
 
 // // タスク完了（MVPでは削除）
