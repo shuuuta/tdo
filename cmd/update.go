b9f186690c6025a6da1db9dc7895fd6c453ae561
Shuta Fujii <shuuutaf@gmail.com>
2025-09-13 14:49:31 +0900

    feat: build update command

---
+++ cmd/update.go
@@ -0,0 +1,104 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/shuuuta/tdo/log"
+	"github.com/shuuuta/tdo/model"
+	"github.com/shuuuta/tdo/project"
+	"github.com/shuuuta/tdo/store"
+	"github.com/spf13/cobra"
+)
+
+var updateGlobal bool
+
+func init() {
+	rootCmd.AddCommand(updateCmd)
+
+	updateCmd.Flags().BoolVarP(&updateGlobal, "global", "g", false, "update global task")
+}
+
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "",
+	Long:  ``,
+	Args:  cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return runUpdate(cmd, args)
+	},
+}
+
+func runUpdate(cmd *cobra.Command, args []string) error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("unable to get working directory: %w", err)
+	}
+
+	pRoot, err := project.DetectRoot(cwd)
+	if err != nil {
+		return fmt.Errorf("unable to detect project root: %w", err)
+	}
+
+	isGlobal := pRoot == "" || updateGlobal
+
+	var p *model.Project
+
+	if isGlobal {
+		if p, err = store.LoadGlobalProject(); err != nil {
+			if os.IsNotExist(err) {
+				cmd.Println("No global tasks found")
+				return nil
+			}
+			return fmt.Errorf("unable to retrieve project tasks: %w\n", err)
+		}
+	} else {
+		if p, err = store.LoadProject(pRoot); err != nil {
+			if os.IsNotExist(err) {
+				cmd.Println("No project tasks found")
+				return nil
+			}
+			return fmt.Errorf("unable to retrieve project tasks: %w\n", err)
+		}
+	}
+
+	title := args[0]
+	targetNum, err := strconv.Atoi(args[1])
+	if err != nil {
+		log.Logf("%s", err.Error())
+		return fmt.Errorf("task ID must be a number: %s", args[1])
+	}
+
+	if targetNum > len(p.Tasks) || targetNum < 0 {
+		return fmt.Errorf("unable to find task: ID %d\n", targetNum)
+	}
+
+	var gotTask *model.Task
+
+	if isGlobal {
+		if gotTask, err = store.UpdateGlobalTask(title, p.Tasks[targetNum-1].ID); err != nil {
+			return err
+		}
+	} else {
+		if gotTask, err = store.UpdateTask(pRoot, title, p.Tasks[targetNum-1].ID); err != nil {
+			return err
+		}
+	}
+
+	var head string
+	if pRoot == "" || updateGlobal {
+		head = "update global task:"
+	} else {
+		head = "update project task:"
+	}
+
+	cmd.Println(head)
+	cmd.Print(viewUpdate(targetNum, gotTask.Title))
+
+	return nil
+}
+
+func viewUpdate(id int, text string) string {
+	return fmt.Sprintf("[ ] %d: %s\n", id, text)
+}
---
+++ cmd/update_test.go
@@ -0,0 +1,178 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/shuuuta/tdo/store"
+)
+
+func TestUpdateTask(t *testing.T) {
+	te := setupTestEnv(t)
+
+	t.Run("Update project task", func(t *testing.T) {
+		if err := os.Chdir(te.ProjectDir); err != nil {
+			t.Fatal(err)
+		}
+		defer te.Cleanup()
+
+		if _, err := store.AddTask(te.ProjectDir, "sample task 1"); err != nil {
+			t.Fatal(err)
+		}
+		if _, err := store.AddTask(te.ProjectDir, "sample task 2"); err != nil {
+			t.Fatal(err)
+		}
+		defer te.Cleanup()
+
+		exp1 := "update test"
+
+		got1, err := executeCommand("update", exp1, "1")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		exp2 := fmt.Sprintf("update project task:\n%s", viewUpdate(1, exp1))
+		if got1 != exp2 {
+			t.Fatalf("\nexpect %q,\ngot    %q", exp2, got1)
+		}
+
+		got2, err := store.LoadProject(te.ProjectDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if got2.Tasks[0].Title != exp1 {
+			t.Fatalf("expect %q, got %q", exp1, got2.Tasks[0].Title)
+		}
+	})
+
+	t.Run("Update global task with -g flag", func(t *testing.T) {
+		if err := os.Chdir(te.ProjectDir); err != nil {
+			t.Fatal(err)
+		}
+		defer te.Cleanup()
+
+		if _, err := store.AddGlobalTask("sample task 1"); err != nil {
+			t.Fatal(err)
+		}
+		if _, err := store.AddGlobalTask("sample task 2"); err != nil {
+			t.Fatal(err)
+		}
+		defer te.Cleanup()
+
+		exp1 := "update test"
+
+		got1, err := executeCommand("update", "-g", exp1, "1")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		exp2 := fmt.Sprintf("update global task:\n%s", viewUpdate(1, exp1))
+		if got1 != exp2 {
+			t.Fatalf("\nexpect %q,\ngot    %q", exp2, got1)
+		}
+
+		got2, err := store.LoadGlobalProject()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if got2.Tasks[0].Title != exp1 {
+			t.Fatalf("expect %q, got %q", exp1, got2.Tasks[0].Title)
+		}
+	})
+
+	t.Run("Update task outside git repo (fallback to global)", func(t *testing.T) {
+		if err := os.Chdir(te.TmpDir); err != nil {
+			t.Fatal(err)
+		}
+		defer te.Cleanup()
+
+		if _, err := store.AddGlobalTask("sample task 1"); err != nil {
+			t.Fatal(err)
+		}
+		if _, err := store.AddGlobalTask("sample task 2"); err != nil {
+			t.Fatal(err)
+		}
+		defer te.Cleanup()
+
+		exp1 := "update test"
+
+		got1, err := executeCommand("update", exp1, "1")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		exp2 := fmt.Sprintf("update global task:\n%s", viewUpdate(1, exp1))
+		if got1 != exp2 {
+			t.Fatalf("\nexpect %q,\ngot    %q", exp2, got1)
+		}
+
+		got2, err := store.LoadGlobalProject()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if got2.Tasks[0].Title != exp1 {
+			t.Fatalf("expect %q, got %q", exp1, got2.Tasks[0].Title)
+		}
+	})
+
+	t.Run("Reject empty task title", func(t *testing.T) {
+		if err := os.Chdir(te.ProjectDir); err != nil {
+			t.Fatal(err)
+		}
+		defer te.Cleanup()
+
+		if _, err := store.AddTask(te.ProjectDir, "sample task 1"); err != nil {
+			t.Fatal(err)
+		}
+		if _, err := store.AddTask(te.ProjectDir, "sample task 2"); err != nil {
+			t.Fatal(err)
+		}
+		defer te.Cleanup()
+
+		_, err1 := executeCommand("update", "  ", "1")
+		if err1 == nil {
+			t.Fatal("expect error when empty args are provided")
+		}
+		exp := "title string is required"
+		if err1.Error() != exp {
+			t.Fatalf("expect %q, got %q", exp, err1.Error())
+		}
+	})
+
+	t.Run("Handle invalid index", func(t *testing.T) {
+		if err := os.Chdir(te.ProjectDir); err != nil {
+			t.Fatal(err)
+		}
+		defer te.Cleanup()
+
+		if _, err := store.AddTask(te.ProjectDir, "sample task 1"); err != nil {
+			t.Fatal(err)
+		}
+		if _, err := store.AddTask(te.ProjectDir, "sample task 2"); err != nil {
+			t.Fatal(err)
+		}
+		defer te.Cleanup()
+
+		_, err1 := executeCommand("update", "sample task", "3")
+		if err1 == nil {
+			t.Fatal("expect error when empty args are provided")
+		}
+		exp1 := "unable to find task: ID 3\n"
+		if err1.Error() != exp1 {
+			t.Fatalf("expect %q, got %q", exp1, err1.Error())
+		}
+
+		_, err2 := executeCommand("update", "sample task", "'-1'")
+		if err2 == nil {
+			t.Fatal("expect error when empty args are provided")
+		}
+		exp2 := "task ID must be a number: '-1'"
+		if err2.Error() != exp2 {
+			t.Fatalf("expect %q, got %q", exp2, err2.Error())
+		}
+	})
+}
